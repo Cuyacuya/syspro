52b0260fc34b8da71d803862ba85fd527ba64868
CHD <vkcclsrn156@naver.com>
2024-10-09 16:18:14 +0000

    check

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -1,34 +1,33 @@
-#include <stdio.h>
-#include "copy.h"
+#include<stdio.h>
+#include<string.h>
+#include"copy.h"
 
-char line[MAXLINE];
-char longest[MAXLINE][MAXLINE];
+char line[5][MAXLINE];
+char copyLine[MAXLINE];
 
-int main() {
-    int len;
-    int max;
-    int Num = 0;
-    max = 0;
+int main(){
+	int i = 0;
 
-    for(int i = 0; i < 5; i++) {
-		gets(line);
-        len = strlen(line);
-        
-        if(len > max) {
-            max = len;
-        }
-        
-        copy(line, longest[Num]);
-        Num++;
-    }
+	while(i<5 && fgets(line[i],MAXLINE,stdin)!=NULL)
+			{
+			
 
-    for(int i = max; i > 0; i--) {
-        for(int j = 0; j < Num; j++) {
-            if(strlen(longest[j]) == i) {
-                printf("%s\n", longest[j]);
-            }
-        }
-    }
+			for(int j = 0; j<i; j++)
+			{
+				if(strlen(line[i])>strlen(line[j]))
+				{
+				copy(line[j],copyLine);
+				copy(line[i],line[j]);
+				copy(copyLine, line[i]);
 
-    return 0;
+				}
+			}
+			i++;
+	}
+
+	printf("\n");
+	for (int k=0; k<i; k++){
+		printf("%s", line[k]);
+				}
+	return 0;
 }
